1e2d88f5f2789a5c0883fa5187f97c6cb5025a8f
LabUser <LabUser@RN213_24>
2017-09-22 19:49:01 -0600

    I have completed challenging mechanics 4 assignment.

--- challenging-mechanics-4-ushapant12345/Form1.cs
+++ challenging-mechanics-4-ushapant12345/Form1.cs
@@ -15,6 +15,508 @@ namespace challenging_mechanics_4_ushapant12345
         public Form1()
         {
             InitializeComponent();
+            string str = "";
+
+
+            int wStepOut = 0, wStepIn = 0;
+            while (wStepOut < 4)
+            {
+                while (wStepIn < 4)
+                {
+                    wStepIn++;
+                }
+                wStepOut++;
+            }
+            for (int oStepOut = 0; oStepOut < 4; oStepOut++)
+            {
+                for (int oStepIn = 0; oStepIn < 4; oStepIn++)
+                {
+                }
+            }
+
+
+            int xStepOut = 0, xStepIn = 0;
+
+            while (xStepOut < 5)
+            {
+                while (xStepIn < 6)
+                {
+                    xStepIn++;
+                }
+                xStepOut++;
+            }
+            for (int pStepOut = 0; pStepOut < 5; pStepOut++)
+            {
+                for (int pStepIn = 0; pStepIn < 5; pStepIn++)
+                {
+                }
+            }
+
+
+            int yStepOut = 1, yStepIn = 1;
+
+            while (yStepOut < 5)
+            {
+                while (yStepIn < 7)
+                {
+                    yStepIn++;
+                }
+                yStepOut++;
+            }
+            for (int qStepOut = 1; qStepOut < 5; qStepOut++)
+            {
+                for (int qStepIn = 1; qStepIn < 7; qStepIn++)
+                {
+                }
+            }
+
+            int zStepOut = 1, zStepIn = 2;
+            while (zStepOut < 4)
+            {
+                while (zStepIn < 10)
+                {
+                    zStepIn++;
+                }
+                zStepOut++;
+            }
+            for (int rStepOut = 1; rStepOut < 4; rStepOut++)
+            {
+                for (int rStepIn = 2; rStepIn < 10; rStepIn++)
+                {
+                }
+            }
+
+
+            int aStepOut = 1, aStepIn = 1;
+
+            while (aStepOut < 11)
+            {
+                while (aStepIn < 11)
+                {
+                    aStepIn++;
+                }
+                aStepOut++;
+            }
+            for (int sStepOut = 1; sStepOut < 11; sStepOut++)
+            {
+                for (int sStepIn = 1; sStepIn < 11; sStepIn++)
+                {
+                }
+            }
+
+
+            int bStepOut = 2, bStepIn = 1;
+
+            while (bStepOut < 11)
+            {
+                while (bStepIn < 4)
+                {
+                    bStepIn++;
+                }
+                bStepOut++;
+            }
+            for (int tStepOut = 2; tStepOut < 11; tStepOut++)
+            {
+                for (int tStepIn = 1; tStepIn < 4; tStepIn++)
+                {
+                }
+            }
+
+
+            int cStepOut = 3, cStepIn = 2;
+
+            while (cStepOut < 10)
+            {
+                while (cStepIn < 11)
+                {
+                    cStepIn+=2;
+                }
+                cStepOut+=3;
+            }
+            for (int lStepOut = 3; lStepOut < 10; lStepOut+=3)
+            {
+                for (int lStepIn = 2; lStepIn < 11; lStepIn+=2)
+                {
+                }
+            }
+
+
+
+
+            int dStepOut = 5, dStepIn = 5;
+
+            while (dStepOut >= 0)
+            {
+                while (dStepIn >= 0)
+                {
+                    dStepIn-- ;
+                }
+                dStepOut--;
+            }
+            for (int mStepOut = 5; mStepOut >=0; mStepOut--)
+            {
+                for (int mStepIn = 5; mStepIn >= 0; mStepIn--)
+                {
+                }
+            }
+
+
+              int eStepOut = 5, eStepIn = 7;
+
+            while (eStepOut >=1)
+            {
+                while (dStepIn >= 1)
+                {
+                    eStepIn--;
+                }
+                eStepOut--;
+            }
+            for (int nStepOut = 5; nStepOut >= 1; nStepOut--)
+            {
+                for (int nStepIn = 5; nStepIn >=1; nStepIn--)
+                {
+                }
+            }
+
+
+
+            int fStepOut = 10, fStepIn = 1;
+
+            while (fStepOut >= 1)
+            {
+                while (fStepIn < 6)
+                {
+                    fStepIn++;
+                }
+                fStepOut--;
+            }
+            for (int oStepOut = 10; oStepOut >= 1; oStepOut--)
+            {
+                for (int oStepIn = 1; oStepIn < 6; oStepIn++)
+                {
+                }
+            }
+
+
+            int gStepOut = 10, gStepIn = 1;
+
+            while (gStepOut >= 2)
+            {
+                while (gStepIn < 14)
+                {
+                    gStepIn+=2;
+                }
+               gStepOut-=2;
+            }
+            for (int pStepOut = 10; pStepOut >= 2; pStepOut-=2)
+            {
+                for (int pStepIn = 1; pStepIn < 14; pStepIn+=2)
+                {
+                }
+            }
+
+
+
+            int hStepOut = 18, hStepIn = 5;
+
+            while (hStepOut >= 3)
+            {
+                while (hStepIn < 76)
+                {
+                    hStepIn+=5;
+                }
+                hStepOut-=2;
+            }
+            for (int qStepOut = 18; qStepOut >= 3; qStepOut-=2)
+            {
+                for (int qStepIn = 5; qStepIn < 76; qStepIn+=5)
+                {
+                }
+            }
+
+
+            int iStepOut = 0, iStepIn = 14;
+
+            while (iStepOut <14)
+            {
+                while (iStepIn >=2)
+                {
+                    iStepIn--;
+                }
+                iStepOut++;
+            }
+            for (int rStepOut = 0; rStepOut <14; rStepOut++)
+            {
+                for (int rStepIn = 14; rStepIn >=2 ; rStepIn--)
+                {
+                }
+            }
+
+            int jStepOut = 1, jStepIn = 300;
+
+            while (jStepOut < 301)
+            {
+                while (jStepIn >= 1)
+                {
+                    jStepIn--;
+                }
+                jStepOut++;
+            }
+            for (int sStepOut = 1; sStepOut < 301; sStepOut++)
+            {
+                for (int sStepIn = 300; sStepIn >= 1; sStepIn--)
+                {
+                }
+            }
+
+
+            int kStepOut = -15, kStepIn = 10;
+
+            while (kStepOut < 14)
+            {
+                while (kStepIn >= -6)
+                {
+                    kStepIn--;
+                }
+                kStepOut++;
+            }
+            for (int tStepOut = -15; tStepOut < 14; tStepOut++)
+            {
+                for (int tStepIn = 10; tStepIn >= -6; tStepIn--)
+                {
+                }
+            }
+
+
+            for (int fOutA = 0; fOutA < 4; fOutA++)
+            {
+                for (int fInA = 0; fInA < 4; fInA++)
+                {
+                    if (fOutA == fInA)
+                    {
+                        str += fOutA;
+                    }
+                }
+            }
+
+
+
+            for (int gOutA = 0; gOutA < 4; gOutA++)
+            {
+                for (int gInA = 0; gInA < 4; gInA++)
+                {
+                    if (gOutA == gInA)
+                    {
+                        str += gOutA;
+                    }
+                }
+            }
+
+
+
+            for (int hOutA = 1;hOutA < 14; hOutA++)
+            {
+                for (int hInA = -6; hInA < 4; hInA++)
+                {
+                    if (hOutA == hInA)
+                    {
+                        str += hOutA;
+                    }
+                }
+            }
+
+
+
+            for (int iOutA = 1; iOutA < 14; iOutA++)
+            {
+                for (int iInA = -13; iInA < 3; iInA++)
+                {
+                    if (iOutA > iInA)
+                    {
+                        str += iOutA;
+                    }
+                }
+            }
+
+
+
+
+            for (int jOutA = 1; jOutA < 14; jOutA++)
+            {
+                for (int jInA = -13; jInA < 3; jInA++)
+                {
+                    if (jOutA > jInA)
+                    {
+                        str += jInA;
+                    }
+                }
+            }
+
+
+
+            for (int kOutA = 1; kOutA < 14; kOutA++)
+            {
+                for (int kInA = 1; kInA < 14; kInA++)
+                {
+                    if (kOutA > kInA)
+                    {
+                        str += kOutA;
+                    }
+                }
+            }
+
+
+            
+            for (int lOutA = 1; lOutA < 101; lOutA++)
+            {
+                for (int lInA = 1; lInA < 11; lInA++)
+                {
+                    if (lOutA % 10 == lInA )
+                    {
+                        str = lOutA + "-" + lInA;
+                    }
+                }
+            }
+
+
+
+            for (int mOutA = 1; mOutA < 145; mOutA++)
+            {
+                for (int mInA = 1; mInA < 13; mInA++)
+                {
+                    if (mOutA % mInA == 0)
+                    {
+                        str += mInA;
+                    }
+                    
+                }
+            }
+
+
+            for (int nOutA = 1; nOutA < 11; nOutA++)
+            {
+                for (int nInA = 1; nInA < 101; nInA++)
+                {
+                    if (nOutA * 10 == nInA)
+                    {
+                        str += nInA;
+                    }
+                }
+            }
+
+
+            for (int oOutA = 1; oOutA < 11; oOutA++)
+            {
+                for (int oInA = 1; oInA < 11; oInA++)
+                {
+                    if ((oOutA * oInA)% 10 == 0 )
+                    {
+                        str += oInA;
+                    }
+                }
+            }
+
+
+            for (int mOutA = 0; mOutA < 4; mOutA++)
+            {
+                for (int mInA = 0; mInA < 4; mInA++)
+                {
+                    str += mOutA + mInA + "|";
+                }
+                str += "\r\n";
+            }
+
+
+
+            for (int nOutA = 1; nOutA < 4; nOutA++)
+            {
+                for (int nInA = 1; nInA < 4; nInA++)
+                {
+                    str += (nOutA-nInA) + "|";
+                }
+                str += "\r\n";
+            }
+
+            for (int oOutA = 1; oOutA < 11; oOutA++)
+            {
+                for (int oInA = 1; oInA < 11; oInA++)
+                {
+                    str += (oOutA - oInA) + "|";
+                }
+                str += "\r\n";
+            }
+
+
+
+            for (int pOutA = 1; pOutA < 11; pOutA++)
+            {
+                for (int pInA = 10; pInA >=1; pInA--)
+                {
+                    str += (pOutA-pInA) + "|";
+                }
+                str += "\r\n";
+            }
+
+
+            for (int rOutA = 0; rOutA < 101; rOutA+=10)
+            {
+                for (int rInA = 0; rInA < 10; rInA++)
+                {
+                    str += rOutA + rInA + "|";
+                }
+                str += "\r\n";
+            }
+
+
+
+            for (int tOutA = 100; tOutA >= 0; tOutA -= 10)
+            {
+                for (int tInA = 9; tInA >= 0; tInA--)
+                {
+                    str += tOutA + tInA + "|";
+                }
+                str += "\r\n";
+            }
+
+
+            for (int uOutA = 10; uOutA < 101; uOutA ++)
+            {
+                for (int uInA = 1; uInA < 11; uInA++)
+                {
+                    str += (uOutA / uInA) + "|";
+                }
+                str += "\r\n";
+            }
+
+            for (int vOutA = 0; vOutA < 11; vOutA ++)
+            {
+                for (int vInA = 1; vInA < 6; vInA++)
+                {
+                    str += (vOutA % vInA) + "|";
+                }
+                str += "\r\n";
+            }
+
+
+            for (int wOutA = 1; wOutA < 11; wOutA += 10)
+            {
+                for (int wInA = 1; wInA < 6; wInA++)
+                {
+                    str += (wOutA ^ wInA) + "|";
+                }
+                str += "\r\n";
+            }
+
+
+            for (int xOutA = 10; xOutA >=1; xOutA--)
+            {
+                for (int xInA = 1; xInA < 6; xInA++)
+                {
+                    str += (xOutA % xInA) + "|";
+                }
+                str += "\r\n";
+            }
+
         }
     }
 }
